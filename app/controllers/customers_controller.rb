444509ef979efec5b8561f2df8527bb8b6d20454
lewiih001 <lewismunyambu@gmail.com>
2023-01-10 19:16:59 +0300

    added private method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,11 +4,21 @@ class CustomersController < ApplicationController
         customers = Customer.all
         render json: customers
     end
-    
+
     def show
         customer = find_customer
         render json: customer
     end
 
+    private
+
+    def customer_params
+      params.permit :first_name, :last_name,  :password, :password_confirmation, :email
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+      render json: { error: invalid.record.errors }, status: :unprocessable_entity
+    end
+  end
 
 end
