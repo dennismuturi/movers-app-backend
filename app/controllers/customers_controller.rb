c35639a1ef732e2625fc60b114ee73faf0f0d03f
lewiih001 <lewismunyambu@gmail.com>
2023-01-10 18:18:51 +0300

    added show and index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,14 @@
 class CustomersController < ApplicationController
+
+    def index
+        customers = Customer.all
+        render json: customers
+    end
+    
+    def show
+        customer = find_customer
+        render json: customer
+    end
+
+
 end
