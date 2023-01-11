ae750854014c436e59e0e3ed3d2fd943fc0e4e04
lewiih001 <lewismunyambu@gmail.com>
2023-01-11 17:39:58 +0300

    added validation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,29 +1,33 @@
 class CustomersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
         customers = Customer.all
-        render json: customers
+        render json: customers, status: :ok
     end
 
     def show
         customer = find_customer
-        render json: customer
+        render json: customer,Serializer:CustomerSerializer status: :ok
     end
 
     def create
         Customer = Customer.create(customer_params)
         render json: customer, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+        
     end
 
     def destroy
         Customer = Customer.find_by(id: params[:id])
         customer.destroy
+        head :no_content
     end
 
     def update
         customer = find_customer
         customer.update!(customer_params)
-        render json: customer, status: :updated
+        render json: customer, status: :ok
     end
 
     private
