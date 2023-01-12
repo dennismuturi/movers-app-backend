1a54112a109adcfe5ec6b063a840eac28d766692
dennismuturi <dennismuturi1@gmail.com>
2023-01-12 10:33:15 +0300

    added validation to vehicle model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_100803) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_230216) do
+  create_table "customer_orders", force: :cascade do |t|
+    t.date "order_date"
+    t.date "delivery_date"
+    t.integer "customer_id"
+    t.integer "mover_id"
+    t.integer "price_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,4 +30,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_100803) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "movers", force: :cascade do |t|
+    t.string "company_name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_available", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "vehicle_name"
+    t.string "vehicle_type"
+    t.string "vehicle_color"
+    t.string "vehicle_number"
+    t.integer "mover_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mover_id"], name: "index_vehicles_on_mover_id"
+  end
+
+  add_foreign_key "vehicles", "movers"
 end
