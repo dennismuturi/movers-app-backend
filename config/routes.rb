876e232c7039e65591ef33fd8ef3d0e4e77deac2
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2023-01-13 19:18:01 +0300

    updated the routes for authenntications

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'movers/mover'
   resources :customers
   resources :customer_orders
-  post 'movers/signup', to: 'movers#create'
-  post 'customer/signup', to: 'customers#create'
+  post 'signup_mover', to: 'movers#create'
+  post 'signup_customer', to: 'customers#create'
 
   resources :vehicles
   resources :movers do
@@ -11,15 +11,13 @@ Rails.application.routes.draw do
   end
  
 
-    get '/login', to: 'sessions#new', as: 'login'
-    post '/movers/login', to: 'movers/sessions#create'
+    post '/movers/login', to: 'sessions#mover_login'
     delete 'movers/logout', to: 'sessions#destroy'
     # other routes for the movers namespace
 
   # Customers routes
 
-    get 'customers/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
+    post '/login', to: 'sessions#customer_login'
     delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
