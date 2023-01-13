fd44e72e9a688a23c9ba021869ca52aab25aac2d
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2023-01-13 19:16:28 +0300

    deleted the sessions controller

--- app/controllers/movers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Movers::SessionsController < ApplicationController
-     def create
-    mover = Mover.find_by(email: params[:email])
-    if mover&& mover.authenticate(params[:password])
-      session[:mover_id] = mover.id
-      render json: mover, status: :created
-    end
-  end
-end
