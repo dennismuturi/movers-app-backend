8da2c253c50e193b71d0eb8136f3105971ebb2ea
dennismuturi <dennismuturi1@gmail.com>
2023-01-17 12:30:56 +0300

    included customer orders to fetched data

--- app/serializers/mover_serializer.rb
+++ app/serializers/mover_serializer.rb
@@ -1,4 +1,5 @@
 class MoverSerializer < ActiveModel::Serializer
-  attributes :id, :company_name, :email, :is_available, :vehicles
+  attributes :id, :company_name, :email, :is_available, :vehicles, :customer_orders
   has_many :vehicles
+  has_many :customer_orders
 end
